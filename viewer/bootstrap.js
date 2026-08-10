7ca7299ddbb8d2b33d189e5715f87faeba878345
language-loop <isorabins@gmail.com>
2026-08-10 22:15:31 +0000

    turn 2248

--- viewer/bootstrap.js
+++ viewer/bootstrap.js
@@ -1,1 +1,1 @@
-window.PUBLIC_BOOTSTRAP = {"turn":2247,"updated":"2026-08-10T22:01:01Z","metrics":[["rulebook revisions","786"],["turns","2247"],["rules adopted","106"],["latest meaning pass \u00b7 V2","FAIL"],["latest semantic coverage \u00b7 V2","93%"],["best message-body savings \u00b7 V2","+61%"]]};
+window.PUBLIC_BOOTSTRAP = {"turn":2248,"updated":"2026-08-10T22:15:28Z","metrics":[["rulebook revisions","787"],["turns","2248"],["rules adopted","106"],["latest meaning pass \u00b7 V2","FAIL"],["latest semantic coverage \u00b7 V2","93%"],["best message-body savings \u00b7 V2","+61%"]]};
